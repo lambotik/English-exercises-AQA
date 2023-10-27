fd5042593dc2d7075f243b555276857d2503259c
lambotik <lambotik1988@gmail.com>
2023-10-28 00:26:31 +0300

    Added: CheckRegression.yml Edit: conftest.py, test_cards.py.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,12 +4,16 @@ import allure
 import pytest
 from selenium import webdriver
 
+from selenium.webdriver.chrome.options import Options
+
 
 @pytest.fixture()
 def driver():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
+    options = Options()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(10)
+    driver.maximize_window()
     yield driver
     attach = driver.get_screenshot_as_png()
     allure.attach(attach, name=f"Screenshot {datetime.today()}", attachment_type=allure.attachment_type.PNG)
