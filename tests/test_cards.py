984ac051261f3a30eb2f52499a986016e4ab771a
lambotik <lambotik1988@gmail.com>
2023-10-30 18:08:26 +0300

    Changed: Edit allure title

--- tests/test_cards.py
+++ tests/test_cards.py
@@ -7,22 +7,21 @@ from pages.chapter1.prepositions.Prepositions import PrepositionsPages
 class TestPrepositions_1_1:
     url = 'https://english.areso.pro/lesson.html?lesson=prepositions_of_time'
 
-    @allure.title('test_check_main_page')
+    @allure.title('Check main page options')
     def test_check_main_options(self, driver):
         main_page = PrepositionsPages(driver, self.url)
         main_page.open()
         main_page.check_main_options(main_page)
-        
-    
-    @allure.title('test_check_questions_no_repeated')
-    @pytest.mark.xfail
+
+    @allure.title('Check questions no repeated')
+    # @pytest.mark.xfail
     def test_check_questions_no_repeated(self, driver):
         main_page = PrepositionsPages(driver, self.url)
         main_page.open()
         data1, data2 = main_page.check_questions_no_repeated(main_page)
         assert data1 == data2, 'Question is repeated'
 
-    @allure.title('test_args_is_not_presence')
+    @allure.title('Check args is not presence')
     def test_args_is_not_presence(self, driver):
         main_page = PrepositionsPages(driver, self.url)
         main_page.open()
@@ -33,18 +32,21 @@ class TestPrepositions_1_1:
 class TestPrepositions_1_3:
     url = 'https://english.areso.pro/lesson.html?lesson=prepositions_in_on_time_at_in_the_end'
 
+    @allure.title('Check main page options')
     def test_check_main_options(self, driver):
         main_page = PrepositionsPages(driver, self.url)
         main_page.open()
         main_page.check_main_options(main_page)
 
-    @pytest.mark.xfail
+    # @pytest.mark.xfail
+    @allure.title('Check questions no repeated')
     def test_check_questions_no_repeated(self, driver):
         main_page = PrepositionsPages(driver, self.url)
         main_page.open()
         data1, data2 = main_page.check_questions_no_repeated(main_page)
         assert data1 == data2, 'Question is repeated'
 
+    @allure.title('Check args is not presence')
     def test_args_is_not_presence(self, driver):
         main_page = PrepositionsPages(driver, self.url)
         main_page.open()
@@ -55,18 +57,21 @@ class TestPrepositions_1_3:
 class TestPrepositions_1_4:
     url = 'https://english.areso.pro/lesson.html?lesson=prepositions_to_at_in_into'
 
+    @allure.title('Check main page options')
     def test_check_main_options(self, driver):
         main_page = PrepositionsPages(driver, self.url)
         main_page.open()
         main_page.check_main_options(main_page)
 
-    @pytest.mark.xfail
+    # @pytest.mark.xfail
+    @allure.title('Check questions no repeated')
     def test_check_questions_no_repeated(self, driver):
         main_page = PrepositionsPages(driver, self.url)
         main_page.open()
         data1, data2 = main_page.check_questions_no_repeated(main_page)
         assert data1 == data2, 'Question is repeated'
 
+    @allure.title('Check args is not presence')
     def test_args_is_not_presence(self, driver):
         main_page = PrepositionsPages(driver, self.url)
         main_page.open()
