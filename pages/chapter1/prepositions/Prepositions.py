476649ef0cabc7980a2ef4cf37b54e797f4c18d4
lambotik <lambotik1988@gmail.com>
2023-10-31 14:49:08 +0300

    Added: fix error

--- pages/chapter1/prepositions/Prepositions.py
+++ pages/chapter1/prepositions/Prepositions.py
@@ -97,7 +97,7 @@ class PrepositionsPages(BasePage):
                 assert result is True, 'Clicked button is not selected'
                 try:
                     assert button_value == correct_answer and correct_or_wrong == 'CORRECT'
-                    print('\n', ex, '\n', '#' * 10, 'Correct answer!', '#' * 10, '\n')
+                    print('\n', '#' * 10, 'Correct answer!', '#' * 10, '\n')
                     # print('#' * 10, "It's right answer.", '#' * 10)
                     # print('\n')
                 except Exception as ex:
