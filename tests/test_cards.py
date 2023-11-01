01a65eb2f70e2051614ae3d0bdcd3a8983264373
lambotik <lambotik1988@gmail.com>
2023-11-01 18:09:58 +0300

    Added: Wrote all tests

--- tests/test_cards.py
+++ tests/test_cards.py
@@ -517,3 +517,146 @@ class TestChapter2:
             main_page = PrepositionsPages(driver, self.url)
             main_page.open()
             main_page.check_args_is_not_presence(main_page)
+
+
+@allure.suite('Chapter 3')
+class TestChapter3:
+    @allure.sub_suite('Test Do or Make.')
+    @allure.link('https://english.areso.pro/lesson.html?lesson=do_or_make')
+    class TestDoOrMake:
+        url = 'https://english.areso.pro/lesson.html?lesson=do_or_make'
+
+        @allure.title('test_check_main_options')
+        @allure.description('Check main page options.')
+        @allure.severity('NORMAL')
+        @allure.id(1)
+        def test_check_main_options(self, driver):
+            main_page = PrepositionsPages(driver, self.url)
+            main_page.open()
+            main_page.check_main_options(main_page)
+
+        @allure.title('test_check_questions_no_repeated')
+        @allure.description('Check questions no repeated.')
+        @allure.severity('NORMAL')
+        @allure.id(2)
+        def test_check_questions_no_repeated(self, driver):
+            main_page = PrepositionsPages(driver, self.url)
+            main_page.open()
+            data1, data2 = main_page.check_questions_no_repeated(main_page)
+            assert data1 == data2, 'Question is repeated'
+
+        @allure.title('test_args_is_not_presence')
+        @allure.description('Check args is not presence.')
+        @allure.severity('NORMAL')
+        @allure.id(3)
+        def test_args_is_not_presence(self, driver):
+            main_page = PrepositionsPages(driver, self.url)
+            main_page.open()
+            main_page.check_args_is_not_presence(main_page)
+
+
+@allure.suite('Chapter 4')
+class TestChapter4:
+    @allure.sub_suite('Test Negative Prefixes')
+    @allure.link('https://english.areso.pro/lesson.html?lesson=negative_prefixes')
+    class TestIrregularVerbs:
+        url = 'https://english.areso.pro/lesson.html?lesson=negative_prefixes'
+
+        @allure.title('test_check_main_options')
+        @allure.description('Check main page options.')
+        @allure.severity('NORMAL')
+        @allure.id(1)
+        def test_check_main_options(self, driver):
+            main_page = IrregularVerbsPages(driver, self.url)
+            main_page.open()
+            main_page.check_irregular_verbs_options()
+
+        @allure.title('test_check_questions_no_repeated')
+        @allure.description('Check questions no repeated.')
+        @allure.severity('NORMAL')
+        @allure.id(2)
+        def test_check_questions_no_repeated(self, driver):
+            main_page = PrepositionsPages(driver, self.url)
+            main_page.open()
+            data1, data2 = main_page.check_questions_no_repeated(main_page)
+            assert data1 == data2, 'Question is repeated'
+
+        @allure.title('test_args_is_not_presence')
+        @allure.description('Check args is not presence.')
+        @allure.severity('NORMAL')
+        @allure.id(3)
+        def test_args_is_not_presence(self, driver):
+            main_page = PrepositionsPages(driver, self.url)
+            main_page.open()
+            main_page.check_args_is_not_presence(main_page)
+
+
+@allure.suite('Chapter 5')
+class TestChapter5:
+    @allure.sub_suite('Test Adjectives.')
+    @allure.link('https://english.areso.pro/lesson.html?lesson=adjectives')
+    class TestIrregularVerbs:
+        url = 'https://english.areso.pro/lesson.html?lesson=adjectives'
+
+        @allure.title('test_check_main_options')
+        @allure.description('Check main page options.')
+        @allure.severity('NORMAL')
+        @allure.id(1)
+        def test_check_main_options(self, driver):
+            main_page = IrregularVerbsPages(driver, self.url)
+            main_page.open()
+            main_page.check_irregular_verbs_options()
+
+        @allure.title('test_check_questions_no_repeated')
+        @allure.description('Check questions no repeated.')
+        @allure.severity('NORMAL')
+        @allure.id(2)
+        def test_check_questions_no_repeated(self, driver):
+            main_page = PrepositionsPages(driver, self.url)
+            main_page.open()
+            data1, data2 = main_page.check_questions_no_repeated(main_page)
+            assert data1 == data2, 'Question is repeated'
+
+        @allure.title('test_args_is_not_presence')
+        @allure.description('Check args is not presence.')
+        @allure.severity('NORMAL')
+        @allure.id(3)
+        def test_args_is_not_presence(self, driver):
+            main_page = PrepositionsPages(driver, self.url)
+            main_page.open()
+            main_page.check_args_is_not_presence(main_page)
+
+@allure.suite('Synonyms')
+class TestSynonyms:
+    @allure.sub_suite('Test synonyms.')
+    @allure.link('https://english.areso.pro/lesson.html?lesson=synonyms')
+    class TestIrregularVerbs:
+        url = 'https://english.areso.pro/lesson.html?lesson=synonyms'
+
+        @allure.title('test_check_main_options')
+        @allure.description('Check main page options.')
+        @allure.severity('NORMAL')
+        @allure.id(1)
+        def test_check_main_options(self, driver):
+            main_page = IrregularVerbsPages(driver, self.url)
+            main_page.open()
+            main_page.check_irregular_verbs_options()
+
+        @allure.title('test_check_questions_no_repeated')
+        @allure.description('Check questions no repeated.')
+        @allure.severity('NORMAL')
+        @allure.id(2)
+        def test_check_questions_no_repeated(self, driver):
+            main_page = PrepositionsPages(driver, self.url)
+            main_page.open()
+            data1, data2 = main_page.check_questions_no_repeated(main_page)
+            assert data1 == data2, 'Question is repeated'
+
+        @allure.title('test_args_is_not_presence')
+        @allure.description('Check args is not presence.')
+        @allure.severity('NORMAL')
+        @allure.id(3)
+        def test_args_is_not_presence(self, driver):
+            main_page = PrepositionsPages(driver, self.url)
+            main_page.open()
+            main_page.check_args_is_not_presence(main_page)
